30083d85dd4a3365a76e6e872e322271011d0945
Karmadibsa <momper.axel.99@gmail.com>
2025-04-24 12:49:15 +0200

    rajout test component

--- src/test/java/org/clubplus/clubplusbackend/ClubPlusBackEndApplicationTests.java
+++ src/test/java/org/clubplus/clubplusbackend/ClubPlusBackEndApplicationTests.java
@@ -1,13 +1,10 @@
 package org.clubplus.clubplusbackend;
 
-import org.clubplus.clubplusbackend.dao.MembreDao;
-import org.clubplus.clubplusbackend.security.SecurityService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -27,10 +24,6 @@ class ClubPlusBackEndApplicationTests {
     private WebApplicationContext context;
     private MockMvc mvc;
 
-    @MockitoBean
-    private SecurityService securityService;
-    @MockitoBean
-    private MembreDao membreRepository;
 
     @BeforeEach
     public void setup() {
@@ -42,34 +35,16 @@ class ClubPlusBackEndApplicationTests {
 
 
     @Test
-    @WithMockUser(username = "bob.membre@email.com", roles = {"MEMBRE"})
+    @WithUserDetails("bob.membre@email.com")
     public void getAdminOfCLub() throws Exception {
 
         mvc.perform(get("/api/clubs/1/admin"))
                 .andExpect(status().isOk());
 
     }
-
-//    @Test
-//    @WithMockUser(username = "bob.membre@email.com", roles = {"MEMBRE"})
-//    public void getAdminOfClub_ShouldReturnOk_WhenMocksAreSetUp() throws Exception {
-//        // --- Arrange ---
-//        Membre mockAdmin = new Membre();
-//        mockAdmin.setId(1);
-//        mockAdmin.setEmail("admin.club1@email.com");
-
-    /// /        /* // Configuration Option B (identique aussi)
-//        doNothing().when(securityService).checkIsCurrentUserMemberOfClubOrThrow(1);
-//        when(membreRepository.findAdminByClubId(1)).thenReturn(Optional.of(mockAdmin));
-//
-//        // --- Act & Assert (identique) ---
-//        mvc.perform(get("/api/clubs/1/admin"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(1))
-//                .andExpect(jsonPath("$.email").value("admin.club1@email.com"));
-//    }
+    
     @Test
-    @WithMockUser(username = "bob.membre@email.com", roles = {"MEMBRE"})
+    @WithUserDetails("bob.membre@email.com")
     public void getAllEventOfMyCLubs() throws Exception {
 
         mvc.perform(get("/api/events"))
